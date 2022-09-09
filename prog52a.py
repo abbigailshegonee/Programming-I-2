f047f380cb0b2ede8e0341359fd09e4a59269799
abbigailshegone <112713146+abbigailshegonee@users.noreply.github.com>
2022-09-09 14:33:43 +0000

    update

---
+++ prog52a.py
@@ -0,0 +1,11 @@
+length = float(input("Please enter the length: "))
+width = float(input("Please enter the length: "))
+
+print("The length is", length)
+print ("The width is", width)
+
+area = length * width
+perim = 2 * length + 2 * width
+
+print("\nThe area is", area)
+print("The perimeter is", perim)
---
+++ prog54b.py
@@ -0,0 +1,4 @@
+first = 475
+secomd =
+third =
+fourth =
