d3b874ed5850b406d44d606d17d042035da51d82
abbigailshegone <112713146+abbigailshegonee@users.noreply.github.com>
2022-09-12 14:39:46 +0000

    update

---
+++ k26.py
@@ -0,0 +1,41 @@
+from stanfordkarel import *
+
+
+def main():
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    put_beeper()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    
+    
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
