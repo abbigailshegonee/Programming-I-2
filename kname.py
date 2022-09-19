73cbd871fc06a70f5291f39d0e30f6962b8853d7
abbigailshegone <112713146+abbigailshegonee@users.noreply.github.com>
2022-09-19 20:58:59 +0000

    update

--- kname.py
+++ kname.py
@@ -4,35 +4,42 @@ class ktools:
   def m(self):
     """shorthand for move"""
     move()
+
     
   def tl(self):
     """turn left"""
     turn_left()
 
+  
   def tr(self):
     """turn right"""
     self.tl()
     self.tl()
     self.tl()
 
+  
   def ta(self):
     """turn around"""
     self.tl()
     self.tl()
 
+  
   def pick(self):
     """pick beeper"""
     pick_beeper()
+    
 
   def put(self):
     """put beeper"""
     put_beeper()
+    
 
   def put2(self):
     """put 2 beepers in a line"""
     self.put()
     self.m()
     self.put()
+    
 
   def put5(self):
     """put 5 beepers in a line"""
@@ -40,12 +47,11 @@ class ktools:
     self.m()
     self.put2()
     self.m()
-    self.put()
+    self.put
+    
 
   def a(self):
-    self.m()
     self.tl()
-    self.m()
     self.put2()
     self.m()
     self.put2()
@@ -123,25 +129,35 @@ class ktools:
     self.put()
     self.m()
     self.m()
-    
-    
 
 
   def g(self):
     self.tl()
     self.m()
     self.put2()
+    self.m()
     self.put()
     self.m()
-    self.ta()
+    self.tr()
     self.m()
     self.put2()
     self.tr()
     self.m()
     self.m()
-    
-    
-    
+    self.tl()
+    self.put2()
+    self.tr()
+    self.m()
+    self.put()
+    self.m()
+    self.tr()
+    self.m()
+    self.put2()
+    self.ta()
+    self.m()
+    self.m()
+    self.m()
+    self.m()
 
 
   def l(self):
@@ -155,10 +171,15 @@ class ktools:
     self.tl()
     self.m()
     self.put2()
-   
   
 
 def main():
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
     kt = ktools()
     kt.a()
     kt.b()
