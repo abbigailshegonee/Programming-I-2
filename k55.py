67ffc46b3685be6a008b05888d320161f3c78b05
abbigailshegone <112713146+abbigailshegonee@users.noreply.github.com>
2022-09-26 14:07:57 +0000

    update

---
+++ k55.py
@@ -0,0 +1,162 @@
+from stanfordkarel import *
+
+
+class ktools:
+  def m(self):
+    """shorthand for move"""
+    move()
+    
+  def tl(self):
+    """turn left"""
+    turn_left()
+
+  def tr(self):
+    """turn right"""
+    self.tl()
+    self.tl()
+    self.tl()
+
+  def ta(self):
+    """turn around"""
+    self.tl()
+    self.tl()
+
+  def pick(self):
+    """pick beeper"""
+    pick_beeper()
+
+  def put(self):
+    """put beeper"""
+    put_beeper()
+
+  def put2(self):
+    """put 2 beepers in a line"""
+    self.put()
+    self.m()
+    self.put()
+
+  def put5(self):
+    """put 5 beepers in a line"""
+    self.put2()
+    self.m()
+    self.put2()
+    self.m()
+    self.put()
+
+  def h(self):
+    """Print H using beepers"""
+    self.tl()
+    self.put5()
+    self.tr()
+    self.m()
+    self.m()
+    self.m()
+    self.tr()
+    self.put5()
+    self.ta()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.put2()
+    self.tl()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.m()
+    self.m()
+    self.m()
+    
+  def fic(self) -> bool:
+    """front is clear"""
+    return front_is_clear()
+    
+    
+  def fid(self) -> bool:
+    """front is blocked"""
+    return not self.fic()
+
+
+  def ric(self) -> bool:
+    """right is clear"""
+    self.tr()
+    if self.fic():
+      self.tl()
+      return True  #ends function there
+    self.tl()
+    return False
+    
+
+  
+  def rib(self) -> bool:
+    """right is blocked"""
+    return not self.ric()
+
+  
+  def mm(self, num):
+    """move multiple"""
+    for number in range(num):
+      self.m()
+
+  
+  def pickm(self, num):
+    """pick multiple"""
+    for i in range(0, num-1):
+      self.pick()
+      self.m()
+    self.pick()
+
+  def putm(self, num):
+    """put multiple"""
+    for _ in range(0, num-1):
+      self.put()
+      self.m()
+    self.put()
+
+  def mput(self, num):
+    """move and put beeper"""
+    for i in range(0, num):
+      self.m()
+      self.put()
+
+  def mtl(self, num):
+    """move and turn left"""
+    for i in range(0, num):
+      self.m()
+      self.tl()
+
+  def tlm(self, num):
+    """turn left and move"""
+    for i in range(0, num):
+      self.tl()
+      self.m()
+
+  def mtr(self, num):
+    """move and turn right"""
+    for i in range(0, num):
+      self.m()
+      self.tr()
+
+  def trm(self, num):
+    """turn right and move"""
+    for i in range(0, num):
+      self.tr()
+      self.m()
+
+  def pickside(self, num):
+    """pick beepers from the side of a rectangle"""
+    for i in range(0,num):
+      self.pickm(4)
+      self.mtl(1)
+
+def main():
+    
+    kt = ktools()
+    kt.pickside(4)
+    
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
