03dab23bb5af37196f1e7908ce8f9bc9da96ebdd
abbigailshegone <112713146+abbigailshegonee@users.noreply.github.com>
2022-11-14 15:09:17 +0000

    update

---
+++ LP4-1.py
@@ -0,0 +1,24 @@
+def main():
+  copies = int(input("Enter number of copies to be printed: "))
+  price = 0.0
+  cost = 0.0
+  if copies > 0 and copies <= 99:
+    price = 0.30
+  elif copies > 99 and copies <= 499:
+    price = 0.28
+  elif copies > 499 and copies <= 749:
+    price = 0.27
+  elif copies > 749 and copies <= 1000:
+    price = 0.26
+  elif copies > 1000:
+    price = 0.25
+  else:
+    print("Invalid number of copies")
+  cost = copies * price
+  print("Price per copy is: " + str(price) + " USD")
+  print("Total cost is: " + str(round(cost, 2)) + " USD")
+  pass
+
+
+if __name__ == "__main__":
+  main()
